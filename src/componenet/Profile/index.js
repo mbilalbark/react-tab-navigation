c85f8ef5c6912ee8dfdc6fd4047a4312c43bd35f
mbilalbark <muh.bark@gmail.com>
2018-10-24 05:56:27 +0300

    Add:Profile Screen added

---
+++ src/componenet/Profile/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Image,View,Text, StyleSheet} from 'react-native'
+
+class Profile extends React.Component {
+
+    render(){
+        return(
+            <View>
+                 <Image style={styles.avatar} source={{uri: 'http://mbilalbark.com/images/profil.jpg'}}/>
+                 <View style={styles.body}>
+                    <Text style={styles.name}>Muhammed Bilal Bark</Text>
+                 </View>
+            </View>
+        )
+    }
+}
+export default Profile
+const styles = StyleSheet.create({
+    avatar: {
+      width: 130,
+      height: 130,
+      borderRadius: 63,
+      borderWidth: 4,
+      borderColor: "white",
+      marginBottom:10,
+      alignSelf:'center',
+      position: 'absolute',
+      marginTop:100
+    },
+    body:{
+      marginTop:130,
+    },
+    name:{
+      fontSize:28,
+      color: "#696969",
+      fontWeight: "600"
+    },
+  });
