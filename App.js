80f2ec6e8476ed52f7d7220467d88797e98806bf
mbilalbark <muh.bark@gmail.com>
2018-10-24 05:54:57 +0300

    Add: Tab Navigation added

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator,createStackNavigator} from 'react-navigation';
+import HomeScreen from './src/componenet/Home'
+import DetailScreen from './src/componenet/Detail'
+import ProfileScreen from './src/componenet/Profile'
+import { Icon } from 'react-native-elements'
+
+const HomeStack = createStackNavigator({
+  Home:{screen: HomeScreen},
+  Detail:{screen: DetailScreen} 
+});
+
+const ProfileStack = createStackNavigator({
+  Profile:{screen: ProfileScreen},
+});
+
+const RootStack = createBottomTabNavigator({
+  Home: {screen: HomeStack},
+  Profile:{screen: ProfileStack}
+},
+{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Home') {
+        iconName = `home${focused ? '' : 'home'}`;
+      } else if (routeName === 'Profile') {
+        iconName = `user${focused ? '' : 'user-alt'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Icon name={iconName} size={25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: '#0c9e9e',
+    inactiveTintColor: '#979797',
+  },
+}
+);
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+  
+        <RootStack/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
