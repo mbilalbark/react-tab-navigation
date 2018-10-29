22c754a7151d957469248d3a3cdc14092f81b8c8
mbilalbark <muh.bark@gmail.com>
2018-10-29 17:21:56 +0300

    Update: Home page navigation params and title update

--- src/componenet/Home/index.js
+++ src/componenet/Home/index.js
@@ -10,13 +10,18 @@ class Home extends React.Component {
         };
     }
 
+    static navigationOptions = {
+          title: 'Home'
+      };
+
   render() {
       return(
           <View 
             style={{backgroundColor:'#ff7e6b'}}
           > 
+          <View style={{marginLeft:50}}>
             <TouchableOpacity 
-             onPress={() => this.props.navigation.navigate('Detail',{asd:'https://i2.wp.com/lbc9.net/movies-and-tv/movie/dark-knight/the-dark-knight-poster-hd-wallpaper.jpg?resize=618%2C348'})}
+             onPress={() => this.props.navigation.navigate('Detail',{url:'https://i2.wp.com/lbc9.net/movies-and-tv/movie/dark-knight/the-dark-knight-poster-hd-wallpaper.jpg?resize=618%2C348',title:'Batman'})}
             > 
                 <Image
                     style={{width: 250, height: 150,margin:5}}
@@ -24,21 +29,22 @@ class Home extends React.Component {
                 />
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Detail',{asd:'https://images5.alphacoders.com/945/thumb-350-945699.jpg'})}
+                onPress={() => this.props.navigation.navigate('Detail',{url:'https://amp.businessinsider.com/images/5bb4cb617654532cc377da8c-750-500.jpg',title:'Venom'})}
             >
                 <Image
                     style={{width: 250, height: 150,margin:5}}
-                    source = {{uri:'https://images5.alphacoders.com/945/thumb-350-945699.jpg'}}
+                    source = {{uri:'https://amp.businessinsider.com/images/5bb4cb617654532cc377da8c-750-500.jpg'}}
                 />
             </TouchableOpacity>
             <TouchableOpacity
-                 onPress={() => this.props.navigation.navigate('Detail',{asd:'https://i.blogs.es/811fce/disney-pixar-up-539902/450_1000.jpg'})}
+                 onPress={() => this.props.navigation.navigate('Detail',{url:'https://i.blogs.es/811fce/disney-pixar-up-539902/450_1000.jpg',title:'Up'})}
             > 
                 <Image
                     style={{width: 250, height: 150, margin:5}}
                     source = {{uri:'https://i.blogs.es/811fce/disney-pixar-up-539902/450_1000.jpg'}}
                 />
             </TouchableOpacity>
+            </View>
           </View>
       )
   }
