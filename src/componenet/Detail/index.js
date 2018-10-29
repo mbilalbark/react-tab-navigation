ef8dbcc7b855b63455f420c52106c552fb5103d2
mbilalbark <muh.bark@gmail.com>
2018-10-29 17:21:25 +0300

    Update: Detail page title updated

--- src/componenet/Detail/index.js
+++ src/componenet/Detail/index.js
@@ -3,18 +3,24 @@ import {View,Image} from 'react-native'
 const url = null;
 class Detail extends React.Component {
 state = {
-    url:null
+    url:null,
 }
+static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.state.params.title
+    };
+  };
+
 componentWillMount() {
-  this.setState({url:this.props.navigation.state.params.asd}) 
-  console.log(url)
+  this.setState({url:this.props.navigation.state.params.url}) 
+  console.log(this.props.navigation)
 }
 
     render() {
         return(
             <View>
                 <Image
-                    style={{width: 250, height: 150,margin:5}}
+                    style={{width: 250, height: 150,margin:5, marginLeft:50}}
                     source = {{uri:this.state.url}}
                 />
             </View>
